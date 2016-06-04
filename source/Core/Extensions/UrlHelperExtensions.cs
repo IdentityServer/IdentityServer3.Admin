0898a013ed993c41140fb36a46588d33161b3fd1
Bert Hoorne <Hoorneb@gmail.com>
2016-06-04 10:17:52 +0200

    Relative url, minor changes.

--- source/Core/Extensions/UrlHelperExtensions.cs
+++ source/Core/Extensions/UrlHelperExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http.Routing;
+﻿using System;
+using System.Web.Http.Routing;
 
 namespace IdentityAdmin.Extensions
 {
@@ -13,7 +14,7 @@ namespace IdentityAdmin.Extensions
         {
             var absoluteUrl = urlHelper.Link(routeName, routeValues);
             var authority = urlHelper.Request.RequestUri.Authority;
-            var authorityIndex = absoluteUrl.IndexOf(urlHelper.Request.RequestUri.Authority);
+            var authorityIndex = absoluteUrl.IndexOf(authority, StringComparison.Ordinal);
             var relativePath = absoluteUrl.Substring(authorityIndex + authority.Length, absoluteUrl.Length - authorityIndex - authority.Length);
             return relativePath;
         }
